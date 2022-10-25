90408ee9ec3d99aaa6614d49886804bd01e1e119
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-10-25 11:17:40 +0530

    component management

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Footer } from '../components/Footer'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <Link href='/about'>About</Link>
       <h1>Homepage</h1>
     </div>
   )
