ba3c7832013dadaa415e20624ed112a049cd84a8
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-10-26 10:11:27 +0530

    added head and title

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,14 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <Image src='/vercel.svg' width={100} height={100} />
-      <h1>Homepage</h1>
-    </div>
+    <>
+      <Head>
+        <title>Alpha 2207</title>
+      </Head>
+      <div>
+        <Image src='/vercel.svg' width={100} height={100} />
+        <h1>Homepage</h1>
+      </div>
+    </>
   )
 }
