dce1b5070f8c2959fa7362cc5bdc0662a4c3c710
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-10-26 10:24:21 +0530

    api call in next app

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
-export default function about() {
+export const getStaticProps = async () => {
+  let res = await fetch('https://jsonplaceholder.typicode.com/users');
+  let data = await res.json();
+  console.log(data);
+  return {
+    props: { ninjas: data }
+  }
+}
+
+export default function about({ ninjas }) {
   return (
     <div>
-        <h1>About</h1>
-        <p>Lorem, ipsum.</p>
+      <h1>About</h1>
+      {ninjas.map(ninja => (
+        <div style={{border:'1px solid red'}}>
+          <h1>{ninja.name}</h1>
+          <p>{ninja.username}</p>
+        </div>
+      )
+      )}
     </div>
   )
 }
