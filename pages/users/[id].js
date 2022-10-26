55ced81f77c970c372d30d9498b7f56f064c8d54
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-10-26 11:11:07 +0530

    advanced dynamic routing

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,11 +1,40 @@
 import React from 'react'
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
 
-const Route = () => {
-  return (
-    <div>
-        <h1>Dynamic Route</h1>
-    </div>
-  )
+    // map data to an array of path objects with params (id)
+    const paths = data.map(ninja => {
+        return {
+            params: { id: ninja.id.toString() }
+        }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+    const data = await res.json();
+
+    return {
+        props: { ninja: data }
+    }
+}
+
+const Route = ({ninja}) => {
+    return (
+        <div style={{border:'1px solid grey',margin:'1rem 0',padding:'1rem'}}>
+            <h1>{ninja.name}</h1>
+            <h5>{ninja.username}</h5>
+            <span>{ninja.email}</span>
+
+        </div>
+    )
 }
 
 export default Route;
