3e7451e485fb9507a2508e271dd7034378b5fec2
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-10-26 10:35:23 +0530

    dynamic route setup

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
         <Link href='/'>Home</Link>
         <Link href='/about'>About</Link>
         <Link href='/contact'>Contact</Link>
-
+        <Link href='/users'>Users</Link>
     </div>
   )
 }
